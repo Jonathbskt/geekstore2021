b84ff11b6b7e3151146d367c00e6b8f006e8411a
jonathbdev <Jonatanbelfiore@outlook.com>
2021-06-29 06:36:17 -0300

    objetos json

--- js/main.js
+++ js/main.js
@@ -1,78 +1,20 @@
 
 //DATOS 
 
- const datosProducts = [
-  {
-    "id":1 ,
-    "nombreProducto": "Auriculares HyperX Cloud Stinger Core 7.1",
-    "precioProducto": 8900,
-    "categoriaProducto": "Auriculares",
-    "categoria" : "Perifericos",
-    "thumbnailUrl": "./img/Perifericos/auris1.jpg"
-  },
-  {
-    "id":2 ,
-    "nombreProducto": "Micrófono Marvo Mic-03 Condenser Usb",
-    "precioProducto": 7338,
-    "categoriaProducto": "Microfonos",
-    "categoria" : "Perifericos",
-    "thumbnailUrl": "./img/Perifericos/MIC-03_800-500x500.jpg"
-  },
-  {
-    "id":3 ,
-    "nombreProducto": "Monitor LG 22MN430H-B 27″ IPS FullHD",
-    "precioProducto": 32370,
-    "categoriaProducto": "Monitores",
-    "categoria" : "Perifericos",
-    "thumbnailUrl": "./img/Perifericos/Z1-500x500.jpg"
-  },
-  {
-    "id":4 ,
-    "nombreProducto": "Notebook Asus X509 i3-1005G1 4GB 1TB 15.6″",
-    "precioProducto": 79560,
-    "categoria" : "Notebooks",
-    "categoriaProducto": "Asus",
-    "thumbnailUrl": "./img/Notebook/note1.jpg"
-  },
-  {
-    "id":5,
-    "nombreProducto": "Geforce 2080",
-    "precioProducto": 150560,
-    "categoria" : "Componentes",
-    "categoriaProducto": "Placas de video",
-    "thumbnailUrl": "./img/Componentes/geforce.jpg"
-  }
+window.onload = function () {
+    const xhttp = new XMLHttpRequest();
 
-]
- 
+    xhttp.open('GET', 'js/api.json', true);
 
+    xhttp.send();
 
+    xhttp.onreadystatechange = function () {
 
-// VARIABLES DATOS - LLAMADO A LOS IMPUTS PARA ADMINISTADOR 
-const nombreProducto = document.querySelector(".nameProduct")
-const precioProducto = document.querySelector(".price") 
-const thumbnailUrl = document.querySelector(".imagenCard")
-//const categoriaProducto = document.querySelector("categoriaProducto") 
+        if (this.readyState == 4 && this.status == 200) {
 
-
-// CONSTRUCTOR
-
-class Product {
-    constructor(nombreProducto, precioProducto, categoriaProducto, categoria, thumbnailUrl) {
-        
-        this.nombreProducto = nombreProducto;
-        this.precioProducto = precioProducto;
-        this.categoriaProducto = categoriaProducto;
-        this.categoria = categoria;
-        this.thumbnailUrl = thumbnailUrl;
-
-    }
-}
-
-
-
-for (const productos of datosProducts) {
-    $("#listadoProduct").append(`<div class="col">
+            let datosProducts = JSON.parse(this.responseText);
+            for (const productos of datosProducts) {
+                $("#listadoProduct").append(`<div class="col">
                         <div class="card h-100">
                             <img src="${productos.thumbnailUrl}" class="card-img-top imagenCard" alt="...">
                             <div class="card-body bg-light">
@@ -85,9 +27,79 @@ for (const productos of datosProducts) {
                             </div>
                         </div>
                     </div> `);
+            }
 
-                    
+            $(".shopingCartClick").click(function () {
+                const thumbnailUrl = document.querySelector(".imagenCard").src
+                const nombreProducto = document.querySelector(".nameProduct").textContent
+                const precioProducto = document.querySelector(".price").textContent
+
+                const product = new Product(nombreProducto, precioProducto, thumbnailUrl);
+                const uiCart = new AddCart();
+
+                uiCart.AddShopingCart(product);
+
+                console.log(thumbnailUrl)
+
+                $(".removeCarrito").click(function () {
+
+                    $("#tableCart").fadeOut('slow')
+                })
+
+            });
+
+            //CUIDADO, AGREGUÉ IMAGEN ESTATICA 
+
+            class AddCart {
+                AddShopingCart(productos) {
+
+                    $(".tablee").append(`<tr id="tableCart">
+
+                <th scope="row"><img class="shopingCartImagen" src="/img/Perifericos/auris1.jpg" alt=""></th> 
+
+                <td class="shopingCartParrafo">${productos.nombreProducto}</td>
+
+                <td><input type="number" class="form-control mb-3" id="shoppingCartCantidad" value="1"></td>
+
+                <td><p class="p-3 price2">${productos.precioProducto}</p>  </td>
+                <td> <button class="removeCarrito"> <i class="fas fa-trash-alt"></i> </button> </td>
+
+
+                </tr>`)
+
+                }
+            }
+
+        }
     }
+}
+
+
+
+// VARIABLES DATOS - LLAMADO A LOS IMPUTS PARA ADMINISTADOR 
+const nombreProducto = document.querySelector(".nameProduct")
+const precioProducto = document.querySelector(".price")
+const thumbnailUrl = document.querySelector(".imagenCard")
+//const categoriaProducto = document.querySelector("categoriaProducto") 
+
+
+// CONSTRUCTOR
+
+class Product {
+    constructor(nombreProducto, precioProducto, categoriaProducto, categoria, thumbnailUrl) {
+
+        this.nombreProducto = nombreProducto;
+        this.precioProducto = precioProducto;
+        this.categoriaProducto = categoriaProducto;
+        this.categoria = categoria;
+        this.thumbnailUrl = thumbnailUrl;
+
+    }
+}
+
+
+
+
 
 // CLASS UI - CARD DINAMICA
 
@@ -112,10 +124,10 @@ class UI {
 
         productList.appendChild(element);
         this.resetForm();
-        
-        
+
+
     }
- 
+
     resetForm() {
         document.getElementById('product-form').reset();
     }
@@ -128,40 +140,21 @@ class UI {
         const container = document.querySelector('.productosCards');
         const app = document.querySelector('#listadoProduct');
         container.insertBefore(div, app);
-        setTimeout(function() {
+        setTimeout(function () {
             document.querySelector('.alert').remove();
         }, 3000)
-        
-
-        
-    }
-}
-
-//CUIDADO, AGREGUÉ IMAGEN ESTATICA 
-class AddCart {
-    AddShopingCart(productos) {
-
-        $(".tablee").append(`<tr id="tableCart">
-
-        <th scope="row"><img class="shopingCartImagen" src="/img/Perifericos/auris1.jpg" alt=""></th> 
-
-        <td class="shopingCartParrafo">${productos.nombreProducto}</td>
-
-        <td><input type="number" class="form-control mb-3" id="shoppingCartCantidad" value="1"></td>
-
-        <td><p class="p-3 price2">${productos.precioProducto}</p>  </td>
-        <td> <button class="removeCarrito"> <i class="fas fa-trash-alt"></i> </button> </td>
 
 
-        </tr>`)
 
     }
 }
+
+
 
 
 //CLICK BOTON SUBIR - LLAMA A LOS IMPUTS ADMINISTRADOR
 
-document.getElementById('product-form').addEventListener('submit',function(event) {
+document.getElementById('product-form').addEventListener('submit', function (event) {
     event.preventDefault();
     let nombreProducto = document.getElementById("nombreProducto").value
     let precioProducto = document.getElementById("precioProducto").value
@@ -176,13 +169,13 @@ document.getElementById('product-form').addEventListener('submit',function(event
 
     ui.agregarProducto(product);
 
-    
+
     ui.showMessage('Producto agregado satisfactoriamente', 'alert alert-success');
 
     sessionStorage.setItem("product", JSON.stringify(product));
 
-    
-}   
+
+}
 )
 
 // VALIDACIÓN DE LOGIN
@@ -194,35 +187,34 @@ let usuario = "user@outlook.com"
 let passwordUsuario = "password"
 
 document.getElementById("loguearme").addEventListener("click", function () {
-    if (document.getElementById("emailLogin").value === administrador && document.getElementById("passwordLogin").value === passwordAdmin ) {
-    
+    if (document.getElementById("emailLogin").value === administrador && document.getElementById("passwordLogin").value === passwordAdmin) {
+
         Swal.fire({
-            title: "Bienvenido Admin!"  
+            title: "Bienvenido Admin!"
         })
         $(".component-menu-lateral-admin").removeClass("hidden-card")
         $(".component-menu-lateral-admin").addClass("visible-card")
-        
-        
 
-    }else if(document.getElementById("emailLogin").value === usuario && document.getElementById("passwordLogin").value === passwordUsuario) 
-    {
+
+
+    } else if (document.getElementById("emailLogin").value === usuario && document.getElementById("passwordLogin").value === passwordUsuario) {
         Swal.fire(
-        {
-            title: "Bienvenido User!"
-            
-        })      	
-         
+            {
+                title: "Bienvenido User!"
+
+            })
+
 
     }
 
     else {
         Swal.fire(
-        {
-            title: "Email y/o contraseña incorrectos",
-        })
+            {
+                title: "Email y/o contraseña incorrectos",
+            })
     }
 
-   
+
 })
 
 
@@ -230,25 +222,6 @@ document.getElementById("loguearme").addEventListener("click", function () {
 
 
 
-   
-$(".shopingCartClick").click(function() {
-    const thumbnailUrl = document.querySelector(".imagenCard").src
-    const nombreProducto = document.querySelector(".nameProduct").textContent
-    const precioProducto = document.querySelector(".price").textContent
-
-    const product = new Product(nombreProducto, precioProducto,thumbnailUrl);
-    const uiCart = new AddCart();
-    
-    uiCart.AddShopingCart(product);
 
 
-     
-    console.log(thumbnailUrl)
-
-    $(".removeCarrito").click(function() {
-
-    $("#tableCart").fadeOut('slow')
-    })
-
-});
 
